003bd3b4d198c53974bbd64d46d991cddd2962cd
Brad Puder <bradleypuder2015@u.northwestern.edu>
2014-04-12 17:51:43 -0500

    api skeleton

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::UsersController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  before_action :cors_preflight_check, :restrict_access
+  after_action :cors_set_access_control_headers
+  respond_to :json
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def transactions
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Havebacon::Application.routes.draw do
 
   resources :initial_users, only: [:create]
 
+  namespace :api do
+    resources :users, only: [:show, :create, :update, :destroy] do
+      member do
+        get "transactions"
+      end
+      resources :goals, only: [:show, :index, :create, :update, :destroy] do
+        resources :transactions, only: [:show, :index, :create, :update, :destroy]
+      end
+    end
+  end
+
 end
